6974cf1f577b508565b220d721894c6812383ffc
student <student@ur.edu.pl>
2025-05-21 17:46:06 +0200

    fix: Poprawiono sposób ladowania glownej sceny.

--- src/main/java/project/app/Main.java
+++ src/main/java/project/app/Main.java
@@ -2,33 +2,25 @@ package project.app;
 
 // Lista import'ów:
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import project.app.utils.ConfirmationHandler;
 import project.app.utils.Constants;
+import project.app.utils.PageManagerUtils;
 
 import java.util.Objects;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("loginPage.fxml"));
-        Parent root = loader.load();
-
+    public void start(Stage primaryStage) {
         primaryStage.setTitle(Constants.appName + " " + Constants.appVersion);
-        primaryStage.setResizable(false);
         primaryStage.getIcons().add(new Image(Objects.requireNonNull(getClass().getResourceAsStream("/images/black_logo.png"))));
-        primaryStage.setScene(new Scene(root));
+        PageManagerUtils.showInitialPage(primaryStage, "/project/app/loginPage.fxml");
         primaryStage.show();
-
         primaryStage.setOnCloseRequest(event -> {
             event.consume();
-            ConfirmationHandler.show("Zamykanie aplikacji", "Czy na pewno chcesz zamknąć aplikację?", primaryStage::close
-            );
+            ConfirmationHandler.show("Zamykanie aplikacji", "Czy na pewno chcesz zamknąć aplikację?", primaryStage::close);
         });
     }
 
