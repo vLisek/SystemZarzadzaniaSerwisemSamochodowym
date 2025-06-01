1288a9e50a219fcc2ba10428fd1c4a0e2d891420
xLisek <filip.lisowski.00@gmail.com>
2025-06-01 18:59:59 +0200

    feat: Dodano kontrolery dla użytkowników. Dodano wyświetlanie imienia, roli i daty na stronie użytkowników.

---
+++ src/main/java/project/app/controllers/ReceptionistPageController.java
@@ -0,0 +1,87 @@
+package project.app.controllers;
+
+import javafx.animation.FadeTransition;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
+import javafx.util.Duration;
+
+import project.app.interfaces.UserDataReceiver;
+import project.app.utils.ConfirmationHandler;
+import project.app.utils.Constants;
+import project.app.utils.PageManagerUtils;
+
+public class ReceptionistPageController implements UserDataReceiver {
+
+    @FXML
+    private StackPane mainPane;
+
+    @FXML
+    private Button logoutButton;
+
+    @FXML
+    private AnchorPane contentPane;
+
+    @FXML
+    private Label nameLabel;
+
+    @FXML
+    private Label positionLabel;
+
+    @FXML
+    private Label dateLabel;
+
+    @FXML
+    public void initialize() {
+        dateLabel.setText("Dziś: " + Constants.getCurrentDate());
+
+        FadeTransition ft = new FadeTransition(Duration.seconds(2), mainPane);
+        ft.setFromValue(0);
+        ft.setToValue(1);
+        ft.play();
+    }
+
+    @FXML
+    private void handleCarsButton() {
+        PageManagerUtils.loadPageIntoAnchorPane("/project/app/carsPage.fxml", contentPane);
+    }
+
+    @FXML
+    private void handleCustomersButton() {
+        PageManagerUtils.loadPageIntoAnchorPane("/project/app/customersPage.fxml", contentPane);
+    }
+
+    @FXML
+    private void handleOrdersButton() {
+        PageManagerUtils.loadPageIntoAnchorPane("/project/app/ordersPage.fxml", contentPane);
+    }
+
+    @FXML
+    private void handleCalendarButton() {
+        PageManagerUtils.loadPageIntoAnchorPane("/project/app/calendarPage.fxml", contentPane);
+    }
+
+    @FXML
+    private void handleInvoicesButton() {
+        PageManagerUtils.loadPageIntoAnchorPane("/project/app/invoicesPage.fxml", contentPane);
+    }
+
+    @FXML
+    private void handleAboutButton() {
+        PageManagerUtils.loadPageIntoAnchorPane("/project/app/aboutPage.fxml", contentPane);
+    }
+
+    @FXML
+    private void handleLogout() {
+        ConfirmationHandler.show("Potwierdzenie", "Czy na pewno chcesz się wylogować?",
+                () -> PageManagerUtils.showPageInSameWindow("/project/app/loginPage.fxml", logoutButton));
+    }
+
+    @Override
+    public void initUserData(String name, String position) {
+        nameLabel.setText("Zalogowany jako: " + name);
+        positionLabel.setText("Stanowisko: " + position);
+    }
+}
---
+++ src/main/java/project/app/controllers/WarehousemanPageController.java
@@ -0,0 +1,67 @@
+package project.app.controllers;
+
+import javafx.animation.FadeTransition;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
+import javafx.util.Duration;
+
+import project.app.interfaces.UserDataReceiver;
+import project.app.utils.ConfirmationHandler;
+import project.app.utils.Constants;
+import project.app.utils.PageManagerUtils;
+
+public class WarehousemanPageController implements UserDataReceiver {
+
+    @FXML
+    private StackPane mainPane;
+
+    @FXML
+    private Button logoutButton;
+
+    @FXML
+    private AnchorPane contentPane;
+
+    @FXML
+    private Label nameLabel;
+
+    @FXML
+    private Label positionLabel;
+
+    @FXML
+    private Label dateLabel;
+
+    @FXML
+    public void initialize() {
+        dateLabel.setText("Dziś: " + Constants.getCurrentDate());
+
+        FadeTransition ft = new FadeTransition(Duration.seconds(2), mainPane);
+        ft.setFromValue(0);
+        ft.setToValue(1);
+        ft.play();
+    }
+
+    @FXML
+    private void handleCarPartsButton() {
+        PageManagerUtils.loadPageIntoAnchorPane("/project/app/carPartsPage.fxml", contentPane);
+    }
+
+    @FXML
+    private void handleAboutButton() {
+        PageManagerUtils.loadPageIntoAnchorPane("/project/app/aboutPage.fxml", contentPane);
+    }
+
+    @FXML
+    private void handleLogout() {
+        ConfirmationHandler.show("Potwierdzenie", "Czy na pewno chcesz się wylogować?",
+                () -> PageManagerUtils.showPageInSameWindow("/project/app/loginPage.fxml", logoutButton));
+    }
+
+    @Override
+    public void initUserData(String name, String position) {
+        nameLabel.setText("Zalogowany jako: " + name);
+        positionLabel.setText("Stanowisko: " + position);
+    }
+}
