f3fc5e2f20d6320267fa97ecf5482fcee54c393d
xLisek <filip.lisowski.00@gmail.com>
2025-06-13 14:14:20 +0200

    fix: poprawka

--- src/main/java/project/app/dao/EmployeeDAO.java
+++ src/main/java/project/app/dao/EmployeeDAO.java
@@ -16,19 +16,7 @@ public class EmployeeDAO {
         List<Employee> employees = new ArrayList<>();
 
         String query = """
-            SELECT 
-                e.employee_id,
-                e.first_name,
-                e.last_name,
-                e.phone_number,
-                e.position,
-                l.login,
-                l.password,
-                r.name as role_name
-            FROM employees e
-            LEFT JOIN logins l ON e.employee_id = l.employee_id
-            LEFT JOIN roles r ON e.role_id = r.id
-            ORDER BY e.employee_id
+            SELECT e.employee_id, e.first_name, e.last_name, e.phone_number, e.position, l.login FROM employees e JOIN logins l ON e.employee_id = l.employee_id ORDER BY e.employee_id
             """;
 
         try {
@@ -44,48 +32,14 @@ public class EmployeeDAO {
                 employee.setPhoneNumber(resultSet.getString("phone_number"));
                 employee.setPosition(resultSet.getString("position"));
                 employee.setLogin(resultSet.getString("login"));
-                employee.setPassword(resultSet.getString("password"));
-                employee.setRoleName(resultSet.getString("role_name"));
 
                 employees.add(employee);
             }
 
         } catch (SQLException e) {
             System.err.println("Błąd podczas pobierania danych pracowników: " + e.getMessage());
-            e.printStackTrace();
         }
 
         return employees;
-    }
-
-    public Employee getEmployeeById(int employeeId) {
-        Employee employee = null;
-
-        String query = "SELECT e.employee_id, e.first_name, e.last_name, e.phone_number, e.position, l.login, l.password, r.name as role_name FROM employees e LEFT JOIN logins l ON e.employee_id = l.employee_id LEFT JOIN roles r ON e.role_id = r.id WHERE e.employee_id = ?";
-
-        try {
-            Connection connection = DatabaseConnector.getConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement(query);
-            preparedStatement.setInt(1, employeeId);
-            ResultSet resultSet = preparedStatement.executeQuery();
-
-            if (resultSet.next()) {
-                employee = new Employee();
-                employee.setEmployeeId(resultSet.getInt("employee_id"));
-                employee.setFirstName(resultSet.getString("first_name"));
-                employee.setLastName(resultSet.getString("last_name"));
-                employee.setPhoneNumber(resultSet.getString("phone_number"));
-                employee.setPosition(resultSet.getString("position"));
-                employee.setLogin(resultSet.getString("login"));
-                employee.setPassword(resultSet.getString("password"));
-                employee.setRoleName(resultSet.getString("role_name"));
-            }
-
-        } catch (SQLException e) {
-            System.err.println("Błąd podczas pobierania pracownika o ID: " + employeeId + " - " + e.getMessage());
-            e.printStackTrace();
-        }
-
-        return employee;
     }
 }
